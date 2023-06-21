1a74ff69fe82134c8d57f16c0ee8faeb03161f35
zcy <134012290+zcy764@users.noreply.github.com>
2023-06-21 17:12:59 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://TrackSantaCC:<Pineapple23!>@cluster0.mifjedx.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://TrackSantaCC:Pineapple23!@cluster0.mifjedx.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
